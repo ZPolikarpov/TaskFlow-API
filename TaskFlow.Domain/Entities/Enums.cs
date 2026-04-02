7b4de917afab6e1c7883b4fce961cbe85b2df2c7
ZPolikarpov <polikarpovzakhar@gmail.com>
2026-04-02 14:22:15 +0200

    Fixed Enums

--- TaskFlow.Domain/Entities/Enums.cs
+++ TaskFlow.Domain/Entities/Enums.cs
@@ -1,18 +1,15 @@
 namespace TaskFlow.Domain.Entities;
-public class Enums
+public enum AppTaskStatus
 {
-    public readonly enum AppTaskStatus
-    {
-        Todo = 0,
-        InProgress = 1,
-        InReview = 2,
-        Done = 3
-    }
-    public readonly enum AppTaskPriority
-    {
-        Low = 0,
-        Medium = 1,
-        High = 2,
-        Critical = 3
-    }
+    Todo = 0,
+    InProgress = 1,
+    InReview = 2,
+    Done = 3
+}
+public enum AppTaskPriority
+{
+    Low = 0,
+    Medium = 1,
+    High = 2,
+    Critical = 3
 }
